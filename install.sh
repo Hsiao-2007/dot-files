755977859a5c1be479743e4f22488e1fb2d1d7d1
Hsiao2007 <147453729+Hsiao-2007@users.noreply.github.com>
2025-01-02 21:32:39 -0500

    Removed wallpapers and started isntall script

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# This is an install script to install all the componenets of this directory
-if [[ "$USER" == "root" ]]; then
-  echo "Root detected\!"
+# This is an install script to install all the components of this directory
+if [[ "$USER" != "root" ]]; then
+  echo "Run this script as root!"
+  exit -1
+else
+  echo -e "!WARNING!\nThis script will install packages using the arch linux pacman, clone repos, and take a while.\n"
+  read -p "Are you sure you want to continue with this script? (y/n): " result
+  if [[ "$result" == "y" || "$result" == "Y" ]]; then
+    # Run all commands here
+    echo "Beginning process"
+  else
+    echo "Cancelling script..."
+    exit -1
+  fi
 fi
-echo "error!"
-
