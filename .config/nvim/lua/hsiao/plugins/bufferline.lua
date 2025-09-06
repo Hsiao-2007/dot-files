98347796f7b296cf609d9d9c2edf5c6c17608845
Hsiao2007 <147453729+Hsiao-2007@users.noreply.github.com>
2025-09-06 15:03:56 -0400

    Changed the file tree to have an icon

--- .config/nvim/lua/hsiao/plugins/bufferline.lua
+++ .config/nvim/lua/hsiao/plugins/bufferline.lua
@@ -8,7 +8,7 @@ return {
 				offsets = {
 					{
 						filetype = "NvimTree",
-						text = "File Explorer",
+						text = "Files ",
 						highlight = "Directory",
 						separator = true -- use a "true" to enable the default, or set your own character
 					}
