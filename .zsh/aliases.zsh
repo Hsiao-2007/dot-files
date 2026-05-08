33a6624c09bcb1eec83bac0979702dc1923b7509
Hsiao2007 <147453729+Hsiao-2007@users.noreply.github.com>
2026-05-08 17:53:32 -0400

    Summer break sysup aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,10 +1,17 @@
 alias gt="git status"
-alias sysup="sudo pacman -Syu; yay -Syu; sudo fwupdtool refresh && sudo fwupdtool upgrade"
+# alias sysup="sudo pacman -Syu; flatpak update; yay -Syu; sudo fwupdtool upgrade"
+alias sysup="sudo pacman -Syu; flatpak update; yay -Syu; sudo fwupdmgr upgrade"
 alias cat="bat"
 alias ls="eza --color=always --git --no-user --no-permissions --no-time --no-filesize --icons=always" 
 alias lt="eza --color=always --git --long --no-user --no-permissions --no-time --no-filesize --tree --level=2 --icons=always"
 alias la="eza --color=always --git --no-user --no-permissions --no-time --no-filesize --icons=always -a" 
 alias ll="eza --color=always --git --long --icons=always"
+alias cp='cp -i'
+alias rm='rm -i'
+alias mv='mv -i'
+alias find='fd --color always'
+alias grep='rg -p'
 alias vim="nvim"
-alias vpnup="sudo nmcli connection up HomeServer"
-alias vpndw="sudo nmcli connection down HomeServer"
+# alias vpnup="sudo nmcli connection up HomeServer"
+# alias vpndw="sudo nmcli connection down HomeServer"
+# alias setup-espidf="source /opt/esp-idf/export.sh"
