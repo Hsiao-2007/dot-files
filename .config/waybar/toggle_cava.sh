edd3dfd1444ca4eb7642f08bc886f36a6fbeaa28
Hsiao2007 <147453729+Hsiao-2007@users.noreply.github.com>
2026-05-14 00:41:47 -0400

    Updated hyprland config Summer 2026

--- .config/waybar/toggle_cava.sh
+++ .config/waybar/toggle_cava.sh
@@ -7,11 +7,10 @@ fi
 if [[ $(cat /tmp/cava) == "off" ]]; then
 	echo "Going on"
 	# Get correct window rules!
-	hyprctl dispatch exec \
-	"[bordersize 0; float; center; workspace special; silent; size 80% 50%] kitty --execute cava"
+	hyprctl dispatch 'hl.dsp.exec_cmd("kitty --execute cava", { border_size = 0, float = true, center = true, workspace = "special",size = {"monitor_w/1.25", "monitor_h/2"}, no_initial_focus = true})'
 	echo on > /tmp/cava
 else
 	echo "Turning off"
-	hyprctl dispatch killwindow "title:cava"
+	killall cava
 	echo off > /tmp/cava
 fi
--- .config/waybar/toggle_clock.sh
+++ .config/waybar/toggle_clock.sh
@@ -7,12 +7,10 @@ fi
 if [[ $(cat /tmp/clock) == "off" ]]; then
 	echo "Going on"
 	# Get correct window rules!
-	hyprctl dispatch exec \
-	"[bordersize 0; float; center; workspace special; silent; size 80% 50%] kitty --execute \
-	peaclock --config-dir ~/.config/peaclock/"
+	hyprctl dispatch 'hl.dsp.exec_cmd("kitty --execute peaclock --config-dir ~/.config/peaclock/",  { border_size = 0, float = true, center = true, workspace = "special",size = {"monitor_w/1.25", "monitor_h/2"}, no_initial_focus = true})'
 	echo on > /tmp/clock
 else
 	echo "Turning off"
-	hyprctl dispatch killwindow "title:peaclock"
+	killall peaclock
 	echo off > /tmp/clock
 fi
