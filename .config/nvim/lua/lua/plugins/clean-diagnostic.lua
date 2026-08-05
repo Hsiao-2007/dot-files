6879e0fe67a9a36218b6ca1c55bc9db1510e0f38
Hsiao2007 <147453729+Hsiao-2007@users.noreply.github.com>
2026-08-05 14:55:30 -0400

    update to file structure

--- .config/nvim/lua/lua/plugins/clean-diagnostic.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"kurama622/clean-diagnostic",
-	event = "LspAttach",
-	opts = {
-		border = "rounded",
-		min_severity = 4,
-		max_width = 78,
-	},
-}
--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -7,7 +7,7 @@ return {
 	build = function()
 		-- build the fuzzy matcher, wait up to 60 seconds
 		-- you can use `gb` in `:Lazy` to rebuild the plugin as needed
-		require('blink.cmp').build():pwait()
+		require('blink.cmp').build():pwait(60000)
 	end,
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
